ce4cb3fdcf8013178c9e23963fda590d5aad3887
ViniciusIth <vini.ithalo@hotmail.com>
2024-06-13 20:10:24 -0300

    fix: player fov (again)

--- ProjectFLorLotus/Assets/Assets/Scripts/Drone/PlayerFOV.cs
+++ ProjectFLorLotus/Assets/Assets/Scripts/Drone/PlayerFOV.cs
@@ -8,43 +8,18 @@ namespace Drone
     [RequireComponent(typeof(Light2D))]
     public class PlayerFOV : MonoBehaviour
     {
-        Transform selfPlayer;
-
-        private void Awake()
-        {
-            selfPlayer = this.transform.parent;
-        }
-
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
         // Update is called once per frame
         void Update()
         {
-            Vector3 pPosition = this.transform.position;
-            this.transform.rotation = Quaternion.FromToRotation(Vector3.up, GetMouseVector()+pPosition);
-        }
-
-        Vector3 GetMouseVector()
-        {
             Vector3 mouseScreenPosition = Input.mousePosition;
+            mouseScreenPosition.z = 10;
+
             Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
 
             Vector3 playerPosition = this.transform.position;
             Vector3 mouseDir = mouseWorldPosition - playerPosition;
-            
 
-            mouseDir.z = 0;
-            Debug.Log(mouseDir);
-            return mouseDir;
-        }
-
-        void SyncFOVDirection(Vector3 mouseDir)
-        {
-
+            this.transform.rotation = Quaternion.FromToRotation(Vector3.up, mouseDir.normalized);
         }
     }
 }
